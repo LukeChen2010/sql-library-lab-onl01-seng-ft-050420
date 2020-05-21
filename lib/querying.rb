ee8a02f38c7188d220746b33c87bad143f118741
Luke Chen <luke.chen.2010@gmail.com>
2020-05-21 05:05:42 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -50,5 +50,5 @@ def select_character_names_and_number_of_books_they_are_in
    INNER JOIN books
    on series.id = books.series_id
    GROUP BY characters.name
-   ORDER BY characters.name;"
+   ORDER BY characters.id;"
 end
