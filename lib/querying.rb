862045bea13f3ffda992fc40fa070c04c3efba3a
Luke Chen <luke.chen.2010@gmail.com>
2020-05-21 04:25:31 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -9,9 +9,8 @@ end
 
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, characters.motto
-  FROM characters
-  WHERE LENGTH(characters.motto) = (SELECT MAX(LENGTH
-  (characters.motto)) FROM characters);"
+   FROM characters
+   ORDER BY LENGTH(characters.motto) DESC LIMIT 1;"
 end
 
 
